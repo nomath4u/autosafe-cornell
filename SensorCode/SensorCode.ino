a41cbd521a2ff9aeab241b9af17ebf0a9f3764f7
Chris Harper <nomath4u@gmail.com>
2014-10-30 21:36:41 -0700

    Added the startup data from the obd2 info into the sensor routine

--- SensorCode/SensorCode.ino
+++ SensorCode/SensorCode.ino
@@ -1,10 +1,26 @@
 #include <Wire.h>
+#include <SoftwareSerial.h>
+
+#define OBDRX 10
+#define OBDTX 11
+
+/******GLOBALS*****/
+//OBDII
+char rxData[20]
+rxIndex=0;
+int vehicleRPM=0; //Used as an example until we decide on commands
+SoftwareSerial obd2(OBDRX,OBDTX);
 
 void setup()
-{
+{ //9DOF
   Wire.begin();
   Serial.begin(9600);
   Serial2.begin(9600);
+  //OBDII
+  obd2.begin(9600);
+  delay(1500); //Have to give it a second to fire up, adapted value may be able to decrease
+  obd2.println("ATZ"); //Get the bus ready
+  delay(2000); //Same as before 
 }
 
 void loop()
