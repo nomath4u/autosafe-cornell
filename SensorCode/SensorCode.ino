298480478b0c3d783143cdcc7ac0660175600948
Chris Harper <nomath4u@gmail.com>
2014-11-05 17:30:34 -0800

    Code now compiles, should work but still unable to test because the hardware isn't around

--- SensorCode/SensorCode.ino
+++ SensorCode/SensorCode.ino
@@ -7,11 +7,11 @@
 
 /******GLOBALS*****/
 //OBDII
-char rxData[20]
-rxIndex=0;
+char rxData[20];
+int rxIndex=0;
 int vehicleRPM=0; //Used as an example until we decide on commands
 SoftwareSerial obd2(OBDRX,OBDTX);
-char commands [NUMCOMMANDS][4] = {"010C"}; //This could be done better,NUMCOMMANDS must be updated
+char commands [NUMCOMMANDS][5] = {"010C"}; //This could be done better,NUMCOMMANDS must be updated, 5 because of null terminator
 
 void setup()
 { //9DOF
@@ -50,5 +50,38 @@ void loop()
     getResponse(); //The actual information we want
     /*Case statement depending on command assuming in loop*/ 
     delay(100); //So we don't query the CANbus too fast and cause a buffer overflow
+  }
+}
+
+   
+//The getResponse function collects incoming data from the UART into the rxData buffer
+// and only exits when a carriage return character is seen. Once the carriage return
+// string is detected, the rxData buffer is null terminated (so we can treat it as a string)
+// and the rxData index is reset to 0 so that the next string can be copied.
+void getResponse(void){
+  char inChar=0;
+  //Keep reading characters until we get a carriage return
+  while(inChar != '\r'){ 
+    //while(1){
+    //If a character comes in on the serial port, we need to act on it.
+    if(obd2.available() > 0){
+      //Start by checking if we've received the end of message character ('\r').
+      if(obd2.peek() == '\r'){
+        //Clear the Serial buffer
+        inChar=obd2.read();
+        //Put the end of string character on our data string
+        rxData[rxIndex]='\0';
+        //Reset the buffer index so that the next character goes back at the beginning of the string.
+        rxIndex=0;
+      }
+      //If we didn't get the end of message character, just add the new character to the string.
+      else{
+        //Get the new character from the Serial port.
+        inChar = obd2.read();
+        //Serial.print(inChar);
+        //Add the new character to the string, and increment the index variable.
+        rxData[rxIndex++]=inChar;
+      }
+    }
   }
 }
