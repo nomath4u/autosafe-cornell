0945bdd88b64fbb3e84781216449f79667bf3669
Chris Harper <nomath4u@gmail.com>
2014-12-03 13:20:23 -0800

    Serial_spewer works, give file name followed by device which is located in the bottom right of the arduino IDE

--- SensorCode/serial_spewer.py
+++ SensorCode/serial_spewer.py
@@ -6,11 +6,16 @@
 from time import sleep
 import serial
 import argparse
-parser = argparse.ArgumentParser()
-#ser = serial.Serial('/dev/tty.usbmodem1d11', 9600) # Establish the connection on a specific port
-data_file = open('data.txt', 'w') #Open the file, clobber existing
+parser = argparse.ArgumentParser(description='Send arduino data to file')
+parser.add_argument("filename", help="name of file to store data")
+parser.add_argument("device", help="path to arduino device")
+args = parser.parse_args()
+print args.filename
+print args.device
+
+ser = serial.Serial(args.device, 9600) # Establish the connection on a specific port
+data_file = open(args.filename, 'w') #Open the file, clobber existing
 while True:
-# data_file.write(ser.readline()) # Read the newest output from the Arduino and send it to the file
-  data_file.write("Test\n")
-  sleep(.1) # Delay for one tenth of a second
+ data_file.write(ser.readline()) # Read the newest output from the Arduino and send it to the file
+# data_file.write("Test\n")
 
