40703ab50e874c04c3cca78fa741bdaf6abe52f3
Sam <quinnsam1@gmail.com>
2015-02-10 09:27:22 -0800

    Fixed mesh

--- mesh.sh
+++ mesh.sh
@@ -18,63 +18,63 @@ while getopts cs option
 
 echo "Stopping Network Manager"
 stop network-manager
-if [ $? ]; then
+if [ ! $? ]; then
     echo "($?)ERROR: Network Manager, did not exit cleanly"
     exit 1
 fi
 
 echo "Enabling the BAT-Signal"
 modprobe batman-adv
-if [ $? ]; then
+if [ ! $? ]; then
     echo "($?)ERROR: BATMAN Kernel module did not load correctly"
     exit 1
 fi
 
 echo "Turning off Wlan0"
 ifconfig wlan0 down
-if [ $? ]; then
+if [ ! $? ]; then
     echo "($?)ERROR: Wlan0 Did not shutdown"
     exit 1
 fi
 
 echo "Configuring MTU"
 ifconfig wlan0 mtu 1528
-if [ $? ]; then
+if [ ! $? ]; then
     echo "($?)ERROR: MTU not set"
     exit 1
 fi
 
 echo "Enabling Ad-Hoc with SSID 'autosafe'"
 iwconfig wlan0 mode Ad-Hoc essid autosafe channel 4 key off
-if [ $? ]; then
+if [ ! $? ]; then
     echo "($?)ERROR: Could not set Ad-hoc parameters"
     exit 1
 fi
 
 echo "Adding Wlan0 to BATMAN"
 batctl if add wlan0
-if [ $? ]; then
+if [ ! $? ]; then
     echo "($?)ERROR: Wlan0 could not attach to bat0"
     exit 1
 fi
 
 echo "Enabling bat0 interface"
 ifconfig bat0 up
-if [ $? ]; then
+if [ ! $? ]; then
     echo "($?)ERROR: Bat0 did not start"
     exit 1
 fi
 
 echo "Enabling Wlan0 interface"
 ifconfig wlan0 up 
-if [ $? ]; then
+if [ ! $? ]; then
     echo "($?)ERROR: Wlan0 did not start"
     exit 1
 fi
 
 echo "Setting ip to $ip"
 ifconfig bat0 10.0.0.$ip
-if [ $? ]; then
+if [ ! $? ]; then
     echo "($?)ERROR: ip Was not set"
     exit 1
 fi
