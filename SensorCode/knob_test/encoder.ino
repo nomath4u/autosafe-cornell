752fe11a65617334bd76d38a7cf58d6adc28c991
greenaca <greenaca@onid.orst.edu>
2015-03-14 13:32:45 -0700

    Updated the knob code to include debouncing for the switch.

--- SensorCode/knob_test/encoder.ino
+++ SensorCode/knob_test/encoder.ino
@@ -7,13 +7,19 @@
 #define one_zero 2
 #define one_one 3
 
+const int buttonPin = 4;    // the number of the pushbutton pin
+int buttonState;             // the current reading from the input pin
+int lastButtonState = LOW;   // the previous reading from the input pin
+long lastDebounceTime = 0;  // the last time the output pin was toggled
+long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
 void setup(){
 
   Serial.begin(9600);
 
   pinMode(2, INPUT_PULLUP);
   pinMode(3, INPUT_PULLUP);
-  pinMode(4, INPUT_PULLUP);
+  pinMode(buttonPin, INPUT_PULLUP);
 }
 
 int concatenate(int x, int y){ 
@@ -37,9 +43,6 @@ int deal_with_encoder(void){
     pin_two = digitalRead(2);
     pin_three = digitalRead(3);
     
-    //pin_two = 1;
-    //pin_three = 1;
- 
     encoder = concatenate(pin_two,pin_three); 
 
     switch(encoder){
@@ -92,10 +95,47 @@ int deal_with_encoder(void){
 void loop()
 {
   int spin = deal_with_encoder();
-  Serial.print("Spin: ");
-  Serial.println(spin);
+  int pressed = digitalRead(buttonPin);
+  
+  //Serial.print("Spin: ");
+//  if(spin == 0){
+//    Serial.println("No Turn");
+//  }else if(spin == 1){
+//    Serial.println("Left Turn");
+//  }else{ 
+//    Serial.println("Right Turn");
+//  }
+  //Serial.println(spin);
   Serial.print("Switch: ");
-  Serial.println(digitalRead(4));
+  if(pressed == 1);{
+        Serial.println("Not Pressed");
+  }
+  
+  // check to see if you just pressed the button 
+  // (i.e. the input went from LOW to HIGH),  and you've waited 
+  // long enough since the last press to ignore any noise:  
+
+  // If the switch changed, due to noise or pressing:
+  if (pressed != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  } 
+  
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer
+    // than the debounce delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (pressed != buttonState) {
+      buttonState = pressed;
+      if(pressed == 0){
+        Serial.println("Pressed");
+      }
+    }
+  }
   delay(100);
+  // save the reading.  Next time through the loop,
+  // it'll be the lastButtonState:
+  lastButtonState = pressed;
 }
 
